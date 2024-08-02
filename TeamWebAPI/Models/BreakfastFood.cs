158bdb2a8d26d15e303aaab28846d7f865ec9a21
Jeffrey Shepard <shepard.jeffrey@gmail.com>
2024-08-02 21:09:38 +0530

    BreakfastFood model updated

--- TeamWebAPI/Models/BreakfastFood.cs
+++ TeamWebAPI/Models/BreakfastFood.cs
@@ -3,10 +3,10 @@ namespace TeamWebAPI.Models
     public class BreakfastFood
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Type { get; set; }
+    public string Name { get; set; }= string.Empty;
+    public string Type { get; set; }= string.Empty;
     public int Calories { get; set; }
-    public string? ServingSize { get; set; }
+    public string ServingSize { get; set; }= string.Empty;
 }
 
 
